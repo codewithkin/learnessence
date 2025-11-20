5a1d870712af895280e7dbb0c119ed711483e751
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-11-20 18:04:50 +0200

    refactor: remove LexicalEditor and related plugins for code cleanup

--- components/editor/editorConfig.ts
+++
@@ -1,3 +0,0 @@
-export const LOCAL_DRAFT_KEY = 'learnessence:note-draft:local';
-export const AUTOSAVE_LOCAL_MS = 1000; // debounce for localStorage autosave
-export const AUTOSAVE_SERVER_MS = 5000; // debounce for server autosave
